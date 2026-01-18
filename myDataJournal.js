6ed7db7d4476fc95dd6ca337dc32a3012be893fc
Vladimir Angelov <v.angelov@hotmail.com>
2026-01-18 14:01:14 +0200

    Add my prediction to the file.

---
+++ myDataJournal.js
@@ -0,0 +1,13 @@
+const weekData = [
+    {day: "Sunday", sleepHours: 7, screenTime: 3, mood: "Chill", caffeineIntake: 2, focusLevel: 5 },
+    {day: "Monday", sleepHours: 6, screenTime: 5.5, mood: "Motivated", caffeineIntake: 1, focusLevel: 8 },
+    {day: "Tuesday", sleepHours: 6.5, screenTime: 0, mood: "Lazy", caffeineIntake: 2, focusLevel: 4 },
+    {day: "Wednesday", sleepHours: 7, screenTime: 4, mood: "Chill", caffeineIntake: 2, focusLevel: 6 },
+    {day: "Thursday", sleepHours: 6.5, screenTime: 5, mood: "Focused", caffeineIntake: 2, focusLevel: 7 },
+    {day: "Friday", sleepHours: 7.5, screenTime: 0, mood: "Hangovered", caffeineIntake: 3, focusLevel: 2 },
+    {day: "Saturday", sleepHours: 7, screenTime: 4.5, mood: "Dedicated", caffeineIntake: 2, focusLevel: 5 },
+]
+
+// I predict that long sleep will not always result in  better screen time and productivity.
+// I predict that coffee not necessarily help me focus better. 
+// I predict that most days I am chill. 
